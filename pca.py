b276e4ca238c381b3b5a3cfe3dd4997b93c7d974
eduardoscsouza <eduardoscsouza@gmail.com>
2017-10-08 16:43:49 -0300

    Add PCA

---
+++ pca.py
@@ -0,0 +1,35 @@
+#Author: Eduardo Santos Carlos de Souza
+
+#Usage:
+#argv[1] = path to npz files with arrays
+#argv[2] = output .npz filename
+
+from sklearn.decomposition import PCA
+import numpy as np
+import os.path
+import sys
+
+in_path = os.path.join("..", "Data", "Datasets", "compressed", "90%", "17flowers")
+out_path = os.path.join("..", "Data", "Accuracies", "17flowers")
+out_size = 4096
+if (len(sys.argv) >= 2):
+	in_path = sys.argv[1]
+if (len(sys.argv) >= 3):
+	out_path = sys.argv[2]
+if (len(sys.argv) >= 4):
+	out_size = int(sys.argv[3])
+
+print("Input: " + in_path)
+print("Output: " + out_path)
+print("Outsize: " + str(out_size))
+
+in_list = np.load(in_path)
+X_list = in_list['arr_0']
+Y_list = in_list['arr_1']
+
+pca = PCA(n_components=out_size)
+out_X_list = pca.fit_transform(X_list)
+
+np.savez(out_path, out_X_list, Y_list)
+print(out_X_list.shape)
+print(Y_list.shape)
