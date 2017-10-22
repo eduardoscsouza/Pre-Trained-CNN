875673c71e67de2bfcef5b4448fee8480e81dd98
eduardoscsouza <eduardoscsouza@gmail.com>
2017-10-22 15:29:10 -0200

    100th time is the charm

--- Shell Scripts/svm_pca_rand.sh
+++ Shell Scripts/svm_pca_rand.sh
@@ -5,8 +5,8 @@ do
 	aux_pow=$((2**$pow))
 	for per in {10..90..20}
 	do
-		find ../DADOS1/esouza/Datasets/reduced/fc2/$aux_pow/$per%/ -type f -name "*rand.npz" -exec bash -c 'python svm.py "{}" ../DADOS1/esouza/Results/SVM_PCA/"$aux_pow"/"$per"%_rand.txt ../DADOS1/esouza/Results/SVM_PCA/"$aux_pow"/"$per"%_rand.npz' \; > ../DADOS1/esouza/Logs/SVM_PCA/svm_pca_rand/$aux_pow/$per%_log.txt 2> ../DADOS1/esouza/Logs/SVM_PCA/svm_pca_rand/$aux_pow/$per%_errlog.txt &
+		find ../DADOS1/esouza/Datasets/reduced/fc2/$aux_pow/$per%/ -type f -name "*rand.npz" -exec bash -c 'python svm.py '"{}"' ../DADOS1/esouza/Results/SVM_PCA/'"$aux_pow"'/'"$per"'%_rand.txt ../DADOS1/esouza/Results/SVM_PCA/'"$aux_pow"'/'"$per"'%_rand.npz' \; > ../DADOS1/esouza/Logs/SVM_PCA/svm_pca_rand/$aux_pow/$per%_log.txt 2> ../DADOS1/esouza/Logs/SVM_PCA/svm_pca_rand/$aux_pow/$per%_errlog.txt &
 	done
-	find ../DADOS1/esouza/Datasets/reduced/fc2/$aux_pow/uncompressed/ -type f -name "*rand.npz" -exec bash -c 'python svm.py "{}" ../DADOS1/esouza/Results/SVM_PCA/"$aux_pow"/uncompressed_rand.txt ../DADOS1/esouza/Results/SVM_PCA/"$aux_pow"/uncompressed_rand.npz' \; > ../DADOS1/esouza/Logs/SVM_PCA/svm_pca_rand/$aux_pow/uncompressed_log.txt 2> ../DADOS1/esouza/Logs/SVM_PCA/svm_pca_rand/$aux_pow/uncompressed_errlog.txt &
+	find ../DADOS1/esouza/Datasets/reduced/fc2/$aux_pow/uncompressed/ -type f -name "*rand.npz" -exec bash -c 'python svm.py '"{}"' ../DADOS1/esouza/Results/SVM_PCA/'"$aux_pow"'/uncompressed_rand.txt ../DADOS1/esouza/Results/SVM_PCA/'"$aux_pow"'/uncompressed_rand.npz' \; > ../DADOS1/esouza/Logs/SVM_PCA/svm_pca_rand/$aux_pow/uncompressed_log.txt 2> ../DADOS1/esouza/Logs/SVM_PCA/svm_pca_rand/$aux_pow/uncompressed_errlog.txt &
 	wait
 done
