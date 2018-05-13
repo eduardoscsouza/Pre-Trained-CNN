f5ee117e2eb6528d5453bfde1bc6363f99bf7636
eduardoscsouza@gmail.com <eduardoscsouza@gmail.com>
2018-05-13 00:14:49 -0300

    Just to be sure

--- Shell Scripts/extract_traindata.sh
+++ Shell Scripts/extract_traindata.sh
@@ -2,6 +2,5 @@
 
 for qual in 1 5 10 25 50 100
 do
-	find ../DADOS1/esouza/Datasets/compressed/$qual/ -mindepth 1 -maxdepth 1 -type d -exec bash -c 'out_filename=$(echo '"{}"' | rev | cut -d'/' -f1 | rev); python extracttrainingdata.py '"{}"' ../DADOS1/esouza/Datasets/training/'"$qual"'/$(echo $out_filename) 5 ' \; > ../DADOS1/esouza/Logs/Train_Data_Extraction/$qual%_log.txt 2> ../DADOS1/esouza/Logs/Train_Data_Extraction/$qual%_errlog.txt &
+	find ../DADOS1/esouza/Datasets/compressed/$qual/ -mindepth 1 -maxdepth 1 -type d -exec bash -c 'out_filename=$(echo '"{}"' | rev | cut -d'/' -f1 | rev); python extracttrainingdata.py '"{}"' ../DADOS1/esouza/Datasets/training/'"$qual"'/$(echo $out_filename) 5 ' \; > ../DADOS1/esouza/Logs/Train_Data_Extraction/$qual%_log.txt 2> ../DADOS1/esouza/Logs/Train_Data_Extraction/$qual%_errlog.txt
 done
-wait
